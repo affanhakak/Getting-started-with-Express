4a3c3e1a58d301c2fca8ef1708cdbbda32a88a41
Affan Hakak <affanshowkat@gmail.com>
2024-02-16 18:11:53 +0530

    query parameters added

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+});
+
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
 });
 
 app.listen(3000, () => {
